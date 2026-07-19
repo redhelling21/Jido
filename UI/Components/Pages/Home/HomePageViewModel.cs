22e74b8184334d833f8c9d1672bcfbe2e9a83f44
BURI Théo <theo.buri@gmail.com>
2026-07-19 16:46:57 +0200

    Added dark mode

--- UI/Components/Pages/Home/HomePageViewModel.cs
+++ UI/Components/Pages/Home/HomePageViewModel.cs
@@ -1,3 +1,7 @@
+using System;
+using Avalonia;
+using CommunityToolkit.Mvvm.ComponentModel;
+using Jido.Config;
 using Jido.Services;
 using Jido.UI.Components;
 using Jido.Utils;
@@ -6,12 +10,32 @@ namespace Jido.UI.Components.Pages.Home
 {
     public partial class HomePageViewModel : ToggleablePageViewModel
     {
+        private readonly JidoConfig? _config;
+
         public string DetectedResolution => $"{ScreenUtils.PrimaryWidth} × {ScreenUtils.PrimaryHeight} px";
+
+        public AppTheme[] Themes { get; } = Enum.GetValues<AppTheme>();
+
+        [ObservableProperty]
+        private AppTheme _selectedTheme;
 
         public HomePageViewModel() { }
 
-        public HomePageViewModel(IMacroService macroService)
+        public HomePageViewModel(IMacroService macroService, JidoConfig config)
             : base(macroService)
-        { }
+        {
+            _config = config;
+            SelectedTheme = config.Theme;
+        }
+
+        partial void OnSelectedThemeChanged(AppTheme value)
+        {
+            if (_config is null || _config.Theme == value)
+                return;
+
+            Application.Current!.RequestedThemeVariant = value.ToVariant();
+            _config.Theme = value;
+            _config.Persist();
+        }
     }
 }
